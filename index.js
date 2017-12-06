902186d515669c0dc08aae084a6a1286b3a5db57
thykos <thykos1994@gmail.com>
2017-12-06 17:17:34 +0200

    added new words to lib

--- index.js
+++ index.js
@@ -1,39 +1,35 @@
 const fs = require('fs');
 const flatten = require('lodash/flatten');
+const tail = require('lodash/tail');
+const clc = require('cli-color');
 const dictionary = require('./lib');
 const files = process.argv.slice(2);
 
-function checkReservedWord(arg) {
-    try {
-        eval(`let ${arg} = 123`);
-        return true;
-    } catch (err) {
-        return false;
-    }
-}
-
 function replaceMurlocWords(field) {
-    if (checkReservedWord(field)) {
         Object.keys(dictionary).forEach(key => {
             const reg = new RegExp(key, 'g');
             field = field.replace(reg, dictionary[key], /g/i);
         });
-    }
     return field;
 }
 
+function isMurloc(filename) {
+    return tail(filename.split('.'))[0] === 'mrls';
+}
+
 function compile(file) {
+    if (!isMurloc(file)) {
+        return;
+    }
     fs.readFile(file, 'utf8', (err, data) => {
         let newData = flatten(data.split('\n').map(item =>
-            flatten(item.split(' ').map(someitem =>
-                flatten(someitem.split('(').map(anyitem => anyitem.split(')')))))
-        ));
-        console.log(newData);
+            flatten(item.split(' '))));
         newData = newData.map(replaceMurlocWords);
-
-        const stream = fs.createWriteStream(file.replace(/mrls/, 'js'));
+        const newFileName = file.replace(/mrls/, 'js');
+        const stream = fs.createWriteStream(newFileName);
         stream.once('open', () => {
             stream.write(newData.join(' '));
+            console.log(`[GRHZR]: ${clc.blue(file)} brghed to`, clc.green(newFileName));
             stream.end();
         });
     });
